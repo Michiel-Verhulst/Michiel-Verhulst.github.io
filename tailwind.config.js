3930e164b681906edce4ceced5c4033eb9b7d3de
Michiel-Verhulst <90454111+Michiel-Verhulst@users.noreply.github.com>
2024-04-19 19:55:47 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                "whisper-white": "#f1f0ee",
+                "deep-abyss": "#293134",
+                "slate-gray": "#434e52",
+                "steel-blue": "#5d6b70",
+                "ash-gray": "#77898e",
+            }
+        }
+    }
+}
